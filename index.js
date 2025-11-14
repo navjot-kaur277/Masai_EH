9b246e13325173b8bffe3e305a42014a95ac5cc5
Navjot Kaur <navjot.kour277@gmail.com>
2025-11-14 23:47:49 +0530

    Add login function

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ function signup(userName) {
     return "Signup Successful, Please Login";
   }
 }
+
+function login(userName, password) {
+  let usernames = ["john", "alice", "emma"]; // existing users
+  const correctPassword = "Emp@123";
+
+  if (!usernames.includes(userName)) {
+    return "User Not Found, Please Signup";
+  } else if (password !== correctPassword) {
+    return "Wrong Password....";
+  } else {
+    return "Login Successful...";
+  }
+}
